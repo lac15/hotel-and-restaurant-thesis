c02e97aa2f3428faef8d4901fce55c79b1ac21db
lac15 <laszlo.erdohegyi@gmail.com>
2017-07-21 13:54:43 +0200

    Modified reserveRoom.xhtml (added startTime and endTime) Modified RoomsMB (saving a new reservation doesn't work properly yet)

--- hotel-web/src/main/java/hu/unideb/inf/thesis/hotel/web/managedbeans/rooms/RoomsMB.java
+++ hotel-web/src/main/java/hu/unideb/inf/thesis/hotel/web/managedbeans/rooms/RoomsMB.java
@@ -1,8 +1,10 @@
 package hu.unideb.inf.thesis.hotel.web.managedbeans.rooms;
 
+import hu.unideb.inf.thesis.hotel.client.api.service.RoomReserveService;
 import hu.unideb.inf.thesis.hotel.client.api.service.RoomService;
 import hu.unideb.inf.thesis.hotel.client.api.service.RoomTypeService;
 import hu.unideb.inf.thesis.hotel.client.api.vo.FoodVo;
+import hu.unideb.inf.thesis.hotel.client.api.vo.RoomReserveVo;
 import hu.unideb.inf.thesis.hotel.client.api.vo.RoomTypeVo;
 import hu.unideb.inf.thesis.hotel.client.api.vo.RoomVo;
 
@@ -10,6 +12,7 @@ import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
 import javax.faces.bean.ManagedBean;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 @ManagedBean(name = "roomsBean")
@@ -17,15 +20,28 @@ public class RoomsMB {
 
     @EJB
     private RoomTypeService roomTypeService;
+    @EJB
+    private RoomReserveService roomReserveService;
 
     private List<RoomTypeVo> roomTypes = new ArrayList<RoomTypeVo>();
     private RoomTypeVo roomType;
+
+    private RoomReserveVo roomReserveVo = new RoomReserveVo();
+
+    private Date startTime = new Date();
+    private Date endTime = new Date();
 
     private String selectedImage;
 
     @PostConstruct
     public void init() {
         roomTypes.addAll(roomTypeService.getRoomTypes());
+    }
+
+    public void addRoomReserve() {
+        roomReserveVo.setStartTime(startTime);
+        roomReserveVo.setEndTime(endTime);
+        roomReserveService.saveRoomReserve(roomReserveVo);
     }
 
     public List<RoomTypeVo> getRoomTypes() {
@@ -42,6 +58,30 @@ public class RoomsMB {
 
     public void setRoomType(RoomTypeVo roomType) {
         this.roomType = roomType;
+    }
+
+    public RoomReserveVo getRoomReserveVo() {
+        return roomReserveVo;
+    }
+
+    public void setRoomReserveVo(RoomReserveVo roomReserveVo) {
+        this.roomReserveVo = roomReserveVo;
+    }
+
+    public Date getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(Date startTime) {
+        this.startTime = startTime;
+    }
+
+    public Date getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(Date endTime) {
+        this.endTime = endTime;
     }
 
     public String getSelectedImage() {
