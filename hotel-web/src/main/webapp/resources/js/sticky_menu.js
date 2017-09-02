cd149a255e4fe5879a4c1c313ddc128762558718
lac15 <laszlo.erdohegyi@gmail.com>
2017-09-02 10:16:22 +0200

    Modified theme.css (added 2% left and right padding to all pages) Modified sticky_menu.js (fixed width problems coming from the padding change)

--- hotel-web/src/main/webapp/resources/js/sticky_menu.js
+++ hotel-web/src/main/webapp/resources/js/sticky_menu.js
@@ -3,10 +3,10 @@ $(document).ready(function(){
 
     $(window).scroll(function(){
         if($(window).scrollTop() > elementPosition.top){
-            $('#navigation').css('position','fixed').css('top','0');
+            $('#navigation').css('position','fixed').css('top','0').css('width', '96%');
             $('#navFix').css('padding-bottom','60px');
         } else {
-            $('#navigation').css('position','static');
+            $('#navigation').css('position','static').css('width', '100%');
             $('#navFix').css('padding-bottom','0');
         }
     });
