08c80ee5e1a61bdf36c184f80e81d6f96f85e765
lac15 <laszlo.erdohegyi@gmail.com>
2017-09-14 09:59:28 +0200

    Added RestaurantMB Modified HotelMB (removed selectedImage) Modified theme.css (removed all !important) Modified restaurant.xhtml (added slideshow and services) Modified restaurant.css (added styleClasses for services) Modified Messages (added messages for restaurant.xhtml)

--- hotel-web/src/main/java/hu/unideb/inf/thesis/hotel/web/managedbeans/restaurant/RestaurantMB.java
+++ hotel-web/src/main/java/hu/unideb/inf/thesis/hotel/web/managedbeans/restaurant/RestaurantMB.java
@@ -1,4 +1,4 @@
-package hu.unideb.inf.thesis.hotel.web.managedbeans.hotel;
+package hu.unideb.inf.thesis.hotel.web.managedbeans.restaurant;
 
 import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
@@ -6,9 +6,9 @@ import javax.faces.bean.RequestScoped;
 import java.util.ArrayList;
 import java.util.List;
 
-@ManagedBean(name = "hotelBean")
+@ManagedBean(name = "restaurantBean")
 @RequestScoped
-public class HotelMB {
+public class RestaurantMB {
 
     private List<String> images;
 
@@ -16,7 +16,7 @@ public class HotelMB {
     public void init() {
         images = new ArrayList<String>();
 
-        for(int i = 1; i <= 5; i++){
+        for(int i = 6; i <= 10; i++){
             images.add("gallery_image_" + i + ".jpg");
         }
     }
